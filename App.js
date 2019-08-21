639fe7e77abd939ea50b6954b4fda61d661e1f25
kgrhartlage <webdevkonto@gmail.com>
2019-08-21 14:33:40 +0200

    Add navigationOptions to screens

--- App.js
+++ App.js
@@ -2,6 +2,19 @@ import { createAppContainer, createStackNavigator } from 'react-navigation';
 import HomeScreen from './src/screens/HomeScreen';
 import SettingsScreen from './src/screens/SettingsScreen';
 
-const AppNavigator = createStackNavigator({ HomeScreen, SettingsScreen });
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      title: 'Home',
+    },
+  },
+  Settings: {
+    screen: SettingsScreen,
+    navigationOptions: {
+      title: 'Settings',
+    },
+  },
+});
 
 export default createAppContainer(AppNavigator);
