2e2517fc99b6bffd22a7d4c498ba5ade8548f4e9
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-08-21 16:09:30 +0200

    Add header styles, and add configuration as comments

--- App.js
+++ App.js
@@ -13,21 +13,27 @@ const AppNavigator = createStackNavigator(
       screen: HomeScreen,
       navigationOptions: {
         title: 'Home',
+        headerBackTitle: 'Start', // back button text (must be on origin screen); works only on iOS; 'null' value will show no text string
       },
     },
     Settings: {
       screen: SettingsScreen,
       navigationOptions: {
         title: 'Settings',
+        headerTintColor: 'white', // font color,
+        // headerTitle: can be used to render a react component as title, e.g. icon. The header prop can receive a react element but not a component,
+        // headerBackImage: display custom image in header back button,
+        // headerRight or headerLeft: to display react element on the right or left side of the title; can replace the back button
       },
     },
   },
   {
     defaultNavigationOptions: {
-      /* handles configuration options for all screens */
+      /* handles configuration options for all screens; values can be overwritten by adding the same property to the navigation options of each screen */
       headerStyle: {
-        backgroundColor: 'hotpink',
+        backgroundColor: 'hotpink', // background color
       },
+      headerTintColor: 'yellow', // font color default
     },
   }
 );
