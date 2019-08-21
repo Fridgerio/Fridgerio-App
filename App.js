8c85c3594e0960c1c59250e0618df8bbe10ac3d4
kgrhartlage <webdevkonto@gmail.com>
2019-08-21 14:40:02 +0200

    Add bottomTabNavigator
    TabBarOption of tintcolor is currently not being recognized by all tabs.

--- App.js
+++ App.js
@@ -1,4 +1,8 @@
-import { createAppContainer, createStackNavigator } from 'react-navigation';
+import {
+  createAppContainer,
+  createStackNavigator,
+  createBottomTabNavigator,
+} from 'react-navigation';
 import HomeScreen from './src/screens/HomeScreen';
 import SettingsScreen from './src/screens/SettingsScreen';
 
@@ -17,4 +21,26 @@ const AppNavigator = createStackNavigator({
   },
 });
 
-export default createAppContainer(AppNavigator);
+const TabNavigator = createBottomTabNavigator(
+  {
+    Home: {
+      screen: AppNavigator,
+      navigationOptions: {
+        title: 'Home',
+      },
+    },
+    Settings: {
+      screen: SettingsScreen,
+      navigationOptions: {
+        title: 'Settings',
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: 'hotpink',
+    },
+  }
+);
+
+export default createAppContainer(TabNavigator);
