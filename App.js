6c88dd6160fb09439d7fc25553321f85facb963a
kgrhartlage <webdevkonto@gmail.com>
2019-08-21 14:28:00 +0200

    Create Home and Settings screens

--- App.js
+++ App.js
@@ -1,19 +1,7 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+import HomeScreen from './src/screens/HomeScreen';
+import SettingsScreen from './src/screens/SettingsScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+const AppNavigator = createStackNavigator({ HomeScreen, SettingsScreen });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default createAppContainer(AppNavigator);
