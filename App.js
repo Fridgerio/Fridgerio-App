6b459c49e5c7c2ae3e1a8b4f5a468581b53d5137
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-08-22 15:23:26 +0200

    finish options for bottomTabNavigator

--- App.js
+++ App.js
@@ -32,10 +32,19 @@ const TabNavigator = createBottomTabNavigator(
   },
   {
     tabBarOptions: {
-      /* TODO: tintColor only working on Home, even when not active */
       activeTintColor: 'hotpink',
+      showLabel: true, // shows the name of the tab (default: true)
+      showIcon: true, // shows the tab icon (default: true)
+      style: {
+        paddingVertical: 20,
+        height: 100,
+      },
+      keyboardHidesTabBar: true, // tab bar always at the bottom, even under the keyboard
     },
   }
+  // navigationOptions: {
+  // tabBarAccessibilityLabel: set a label for screen readers if tabTitle is not set
+  // },
 );
 
 export default createAppContainer(TabNavigator);
