b2a93d029b4b1980567e3de6cce191a3a23548e5
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-08-28 22:57:58 +0200

    Remove default styles and header options

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 function SettingsScreen() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Hello from Settings</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-  },
-});
-
-SettingsScreen.navigationOptions = {
-  header: null,
-};
 
 export default SettingsScreen;
