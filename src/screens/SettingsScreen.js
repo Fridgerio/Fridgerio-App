074470c4acadae80471715cf8de4ed74a0232e7d
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-08-29 00:33:25 +0200

    Add styles (styled-components)

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,31 +1,117 @@
 import React from 'react';
-import { ScrollView, View, Text, Linking } from 'react-native';
+import { ScrollView, Linking } from 'react-native';
 import ToggleSwitch from 'toggle-switch-react-native';
+import styled from 'styled-components';
 
 function SettingsScreen() {
   return (
     <ScrollView>
-      <View>
-        <ToggleSwitch isOn label="Sprache" />
-        <ToggleSwitch isOn label="Design" />
-        <Text>Benachrichtigungen</Text>
-      </View>
-      <View>
-        <Text>Bewerte die App</Text>
-        <Text>Teile die App</Text>
-      </View>
-      <View>
-        <Text
+      <SettingsView>
+        <LanguageLink>Sprache</LanguageLink>
+        <ThemeLink>
+          <ThemeLabel>Design</ThemeLabel>
+          <ToggleButton>
+            <ToggleSwitch isOn onColor="hotpink" />
+          </ToggleButton>
+        </ThemeLink>
+        <NotificationsLink>Benachrichtigungen</NotificationsLink>
+      </SettingsView>
+      <SocialLinks>
+        <RateLink>Bewerte die App</RateLink>
+        <ShareLink>Teile die App</ShareLink>
+      </SocialLinks>
+      <HelpView>
+        <HelpLink
           onPress={() =>
             Linking.openURL('https://facebook.github.io/react-native/docs/linking')}
         >
           Hilfe / FAQ
-        </Text>
-        <Text>Impressum</Text>
-        <Text>Datenschutzerklärung</Text>
-      </View>
+        </HelpLink>
+      </HelpView>
+      <LegalLinks>
+        <LegalNoticeLink>Impressum</LegalNoticeLink>
+        <PrivacyPolicyLink>Datenschutzerklärung</PrivacyPolicyLink>
+      </LegalLinks>
     </ScrollView>
   );
 }
 
 export default SettingsScreen;
+
+const SettingsView = styled.View`
+  padding: 5px 10px;
+  margin: 5px 0;
+  border-bottom-width: 1px;
+  border-bottom-color: #f0f0f0;
+`;
+
+const LanguageLink = styled.Text`
+  padding: 4px 0;
+  font-size: 16px;
+`;
+
+const ThemeLink = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const ThemeLabel = styled.Text`
+  padding: 4px 0;
+  font-size: 16px;
+`;
+
+const ToggleButton = styled.View`
+  padding: 4px 0;
+  text-align: right;
+`;
+
+const NotificationsLink = styled.Text`
+  padding: 4px 0;
+  font-size: 16px;
+`;
+
+const SocialLinks = styled.View`
+  padding: 5px 10px;
+  margin: 5px 0;
+  border-bottom-width: 1px;
+  border-bottom-color: #f0f0f0;
+`;
+
+const RateLink = styled.Text`
+  padding: 4px 0;
+  font-size: 16px;
+`;
+
+const ShareLink = styled.Text`
+  padding: 4px 0;
+  font-size: 16px;
+`;
+
+const HelpView = styled.View`
+  padding: 5px 10px;
+  margin: 5px 0;
+  border-bottom-width: 1px;
+  border-bottom-color: #f0f0f0;
+`;
+
+const HelpLink = styled.Text`
+  padding: 4px 0;
+  font-size: 16px;
+`;
+
+const LegalLinks = styled.View`
+  padding: 5px 10px;
+  margin: 5px 0;
+  border-bottom-width: 1px;
+  border-bottom-color: #f0f0f0;
+`;
+
+const LegalNoticeLink = styled.Text`
+  padding: 4px 0;
+  font-size: 16px;
+`;
+
+const PrivacyPolicyLink = styled.Text`
+  padding: 4px 0;
+  font-size: 16px;
+`;
