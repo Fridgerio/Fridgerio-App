64d55aca5396a8cef924be7483b3781265bb53b8
kgrhartlage <webdevkonto@gmail.com>
2019-08-28 22:49:12 +0200

    Create dummy component

---
+++ src/components/DummyComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function DummyComponent() {
+  return (
+    <View>
+      <Text>
+        A few hours of trial & error can save you several minutes of looking at
+        the README.
+      </Text>
+    </View>
+  );
+}
