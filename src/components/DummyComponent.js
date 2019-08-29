f43637d8b28160db801123091759333c50e40cad
kgrhartlage <webdevkonto@gmail.com>
2019-08-29 08:48:31 +0200

    Test platform-specific code

--- src/components/DummyComponent.js
+++ src/components/DummyComponent.js
@@ -1,13 +1,43 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Platform, StyleSheet } from 'react-native';
+
+function ComponentIOS() {
+  return <Text>You are using iOS 👍</Text>;
+}
+
+function ComponentAndroid() {
+  return <Text>You are using Android 👎</Text>;
+}
+
+const Component = Platform.select({
+  // ios: () => require('ComponentIOS')
+  ios: () => ComponentIOS,
+  android: () => ComponentAndroid,
+})();
 
 export default function DummyComponent() {
   return (
     <View>
-      <Text>
+      <Text style={styles.text}>
         A few hours of trial & error can save you several minutes of looking at
         the README.
       </Text>
+      <Component />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    paddingVertical: 30,
+    color: Platform.OS === 'ios' ? 'darkorange' : 'deeppink',
+    ...Platform.select({
+      ios: {
+        textTransform: 'uppercase',
+      },
+      android: {
+        textTransform: 'capitalize',
+      },
+    }),
+  },
+});
