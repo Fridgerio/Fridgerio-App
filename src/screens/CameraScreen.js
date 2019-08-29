dec908afbb7ada55aeddcb122efb8b9297bd1162
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-08-29 13:21:07 +0200

    cleanup

--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -1,36 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Text, Button } from 'react-native';
-import { Camera } from 'expo-camera';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import * as Permissions from 'expo-permissions';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { Ionicons } from '@expo/vector-icons';
 
 function CameraScreen() {
-  // // const [flash, setFlash] = useState(false);
-  // const [type] = useState('back');
-  // // const toggleFlash = () => setFlash(!flash);
-  // // const renderTopBar = () => (
-  // // <View>
-  // // <TouchableOpacity onPress={toggleFlash}>
-  // // <Ionicons name={'ion-flash'} size={32} color="white" />
-  // // </TouchableOpacity>
-  // // </View>
-  // // );
-  // const renderCamera = () => (
-  //   <View style={{ flex: 1 }}>
-  //     <Camera
-  //       ref={ref => {
-  //         const camera = ref;
-  //       }}
-  //       type={type}
-  //       // flashMode={flash}
-  //     >
-  //       {/* {renderTopBar()} */}
-  //     </Camera>
-  //   </View>
-  // );
-  // return <View style={styles.container}>{renderCamera()}</View>;
   const [hasCameraPermission, toggleCameraPermission] = useState(null);
   const [scanned, toggleScanned] = useState(false);
   useEffect(() => {
