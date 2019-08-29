1f086c07b8dbe2d428c553d921f7e874866e14e6
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-08-29 15:24:49 +0200

    Add sample notification time frame picker

--- src/screens/SettingsNotificationsScreen.js
+++ src/screens/SettingsNotificationsScreen.js
@@ -1,20 +1,149 @@
 import React from 'react';
-import { ScrollView, Text } from 'react-native';
+import { ScrollView, Text, Picker } from 'react-native';
 
 function SettingsNotificationsScreen() {
   return (
     <ScrollView>
       <Text>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-        ipsum dolor sit amet.
+        Hier kannst Du die default-Benachrichtigungszeiträume für jede
+        Lebensmittel-Kategorie anpassen.
       </Text>
+
+      <Text>Kategorie 1</Text>
+      <Picker selectedValue={14}>
+        <Picker.Item label="1" value={1} />
+        <Picker.Item label="2" value={2} />
+        <Picker.Item label="3" value={3} />
+        <Picker.Item label="4" value={4} />
+        <Picker.Item label="5" value={5} />
+        <Picker.Item label="6" value={6} />
+        <Picker.Item label="7" value={7} />
+        <Picker.Item label="8" value={8} />
+        <Picker.Item label="9" value={9} />
+        <Picker.Item label="10" value={10} />
+        <Picker.Item label="11" value={11} />
+        <Picker.Item label="12" value={12} />
+        <Picker.Item label="13" value={13} />
+        <Picker.Item label="14" value={14} />
+        <Picker.Item label="15" value={15} />
+        <Picker.Item label="16" value={16} />
+        <Picker.Item label="17" value={17} />
+        <Picker.Item label="18" value={18} />
+        <Picker.Item label="19" value={19} />
+        <Picker.Item label="20" value={20} />
+        <Picker.Item label="21" value={21} />
+        <Picker.Item label="22" value={22} />
+        <Picker.Item label="23" value={23} />
+        <Picker.Item label="24" value={24} />
+        <Picker.Item label="25" value={25} />
+        <Picker.Item label="26" value={26} />
+        <Picker.Item label="27" value={27} />
+        <Picker.Item label="28" value={28} />
+        <Picker.Item label="29" value={29} />
+        <Picker.Item label="30" value={30} />
+      </Picker>
+
+      <Text>Kategorie 2</Text>
+      <Picker selectedValue={7}>
+        <Picker.Item label="1" value={1} />
+        <Picker.Item label="2" value={2} />
+        <Picker.Item label="3" value={3} />
+        <Picker.Item label="4" value={4} />
+        <Picker.Item label="5" value={5} />
+        <Picker.Item label="6" value={6} />
+        <Picker.Item label="7" value={7} />
+        <Picker.Item label="8" value={8} />
+        <Picker.Item label="9" value={9} />
+        <Picker.Item label="10" value={10} />
+        <Picker.Item label="11" value={11} />
+        <Picker.Item label="12" value={12} />
+        <Picker.Item label="13" value={13} />
+        <Picker.Item label="14" value={14} />
+        <Picker.Item label="15" value={15} />
+        <Picker.Item label="16" value={16} />
+        <Picker.Item label="17" value={17} />
+        <Picker.Item label="18" value={18} />
+        <Picker.Item label="19" value={19} />
+        <Picker.Item label="20" value={20} />
+        <Picker.Item label="21" value={21} />
+        <Picker.Item label="22" value={22} />
+        <Picker.Item label="23" value={23} />
+        <Picker.Item label="24" value={24} />
+        <Picker.Item label="25" value={25} />
+        <Picker.Item label="26" value={26} />
+        <Picker.Item label="27" value={27} />
+        <Picker.Item label="28" value={28} />
+        <Picker.Item label="29" value={29} />
+        <Picker.Item label="30" value={30} />
+      </Picker>
+
+      <Text>Kategorie 3</Text>
+      <Picker selectedValue={30}>
+        <Picker.Item label="1" value={1} />
+        <Picker.Item label="2" value={2} />
+        <Picker.Item label="3" value={3} />
+        <Picker.Item label="4" value={4} />
+        <Picker.Item label="5" value={5} />
+        <Picker.Item label="6" value={6} />
+        <Picker.Item label="7" value={7} />
+        <Picker.Item label="8" value={8} />
+        <Picker.Item label="9" value={9} />
+        <Picker.Item label="10" value={10} />
+        <Picker.Item label="11" value={11} />
+        <Picker.Item label="12" value={12} />
+        <Picker.Item label="13" value={13} />
+        <Picker.Item label="14" value={14} />
+        <Picker.Item label="15" value={15} />
+        <Picker.Item label="16" value={16} />
+        <Picker.Item label="17" value={17} />
+        <Picker.Item label="18" value={18} />
+        <Picker.Item label="19" value={19} />
+        <Picker.Item label="20" value={20} />
+        <Picker.Item label="21" value={21} />
+        <Picker.Item label="22" value={22} />
+        <Picker.Item label="23" value={23} />
+        <Picker.Item label="24" value={24} />
+        <Picker.Item label="25" value={25} />
+        <Picker.Item label="26" value={26} />
+        <Picker.Item label="27" value={27} />
+        <Picker.Item label="28" value={28} />
+        <Picker.Item label="29" value={29} />
+        <Picker.Item label="30" value={30} />
+      </Picker>
+
+      <Text>Kategorie 4</Text>
+      <Picker selectedValue={14}>
+        <Picker.Item label="1" value={1} />
+        <Picker.Item label="2" value={2} />
+        <Picker.Item label="3" value={3} />
+        <Picker.Item label="4" value={4} />
+        <Picker.Item label="5" value={5} />
+        <Picker.Item label="6" value={6} />
+        <Picker.Item label="7" value={7} />
+        <Picker.Item label="8" value={8} />
+        <Picker.Item label="9" value={9} />
+        <Picker.Item label="10" value={10} />
+        <Picker.Item label="11" value={11} />
+        <Picker.Item label="12" value={12} />
+        <Picker.Item label="13" value={13} />
+        <Picker.Item label="14" value={14} />
+        <Picker.Item label="15" value={15} />
+        <Picker.Item label="16" value={16} />
+        <Picker.Item label="17" value={17} />
+        <Picker.Item label="18" value={18} />
+        <Picker.Item label="19" value={19} />
+        <Picker.Item label="20" value={20} />
+        <Picker.Item label="21" value={21} />
+        <Picker.Item label="22" value={22} />
+        <Picker.Item label="23" value={23} />
+        <Picker.Item label="24" value={24} />
+        <Picker.Item label="25" value={25} />
+        <Picker.Item label="26" value={26} />
+        <Picker.Item label="27" value={27} />
+        <Picker.Item label="28" value={28} />
+        <Picker.Item label="29" value={29} />
+        <Picker.Item label="30" value={30} />
+      </Picker>
     </ScrollView>
   );
 }
