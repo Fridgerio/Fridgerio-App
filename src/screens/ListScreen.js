e541ef9d00eb034e980e18278baaa9b684011151
kgrhartlage <webdevkonto@gmail.com>
2019-08-30 09:19:08 +0200

    Hide scroll indicator

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -31,6 +31,7 @@ function CategoryFilter() {
     <FlatList
       style={styles.categoryList}
       horizontal
+      showsHorizontalScrollIndicator={false}
       data={['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']}
       keyExtractor={category => category}
       renderItem={() => (
