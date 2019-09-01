c93108090e09cde6cc3443a78f8dada83f5c5e22
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-09-01 17:42:39 +0200

    Refactor the theme settings screen
    Rename components and outsource styling

--- src/screens/SettingsThemeScreen.js
+++ src/screens/SettingsThemeScreen.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import { ScrollView } from 'react-native';
-import styled from 'styled-components';
-import { Ionicons } from '@expo/vector-icons';
 
 function SettingsThemeScreen() {
   return (
     <ScrollView>
-      <ThemeLinks>
-        <ThemeLink>
-          <ThemeLabel>Light</ThemeLabel>
-        </ThemeLink>
-        <ThemeLink>
-          <ThemeLabel>Dark</ThemeLabel>
-          <Ionicons name="md-checkmark" size={24} />
-        </ThemeLink>
-      </ThemeLinks>
+      <Textbox>
+        <RowLink title="Light" name="md-checkmark" />
+        <RowLink title="Dark" />
+      </Textbox>
     </ScrollView>
   );
 }
@@ -24,20 +17,3 @@ SettingsThemeScreen.navigationOptions = {
 };
 
 export default SettingsThemeScreen;
-
-const ThemeLinks = styled.View`
-  padding: 5px 15px;
-  margin: 5px 0;
-  border-bottom-width: 1px;
-  border-bottom-color: #f0f0f0;
-`;
-
-const ThemeLink = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
-`;
-
-const ThemeLabel = styled.Text`
-  padding: 4px 0;
-  font-size: 16px;
-`;
