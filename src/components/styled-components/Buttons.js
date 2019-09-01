6d01bb48caea8b4e51b5e9ce23fe4476d0f2c9ba
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-09-01 11:40:19 +0200

    Add styling for default button and adjust save button

--- src/components/styled-components/Buttons.js
+++ src/components/styled-components/Buttons.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// Save Button
-const StyledSaveButton = styled.TouchableOpacity`
+// Default Button
+const StyledDefaultButton = styled.TouchableOpacity`
   background-color: #dbd962;
   padding: 3px 5px;
+  margin: 3px 0;
   width: 40%;
   border-radius: 5px;
 `;
-const StyledSaveButtonText = styled.Text`
+const StyledDefaultButtonText = styled.Text`
   text-align: center;
   font-size: 18px;
 `;
+export const DefaultButton = props => (
+  <StyledDefaultButton {...props}>
+    <StyledDefaultButtonText>{props.title}</StyledDefaultButtonText>
+  </StyledDefaultButton>
+);
+
+// Save Button
+const StyledSaveButton = styled(DefaultButton)`
+  background-color: #3cb371;
+`;
+const StyledSaveButtonText = styled(DefaultButton)``;
 export const SaveButton = props => (
   <StyledSaveButton {...props}>
     <StyledSaveButtonText>{props.title}</StyledSaveButtonText>
