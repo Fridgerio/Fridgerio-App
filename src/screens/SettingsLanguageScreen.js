447d7456dd4873834158416124903771caec451a
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-09-01 17:42:18 +0200

    Refactor the language settings screen
    Rename components and outsource styling

--- src/screens/SettingsLanguageScreen.js
+++ src/screens/SettingsLanguageScreen.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import { ScrollView } from 'react-native';
-import styled from 'styled-components';
-import { Ionicons } from '@expo/vector-icons';
+import { Textbox } from '../components/styled-components/Boxes';
+import { RowLink } from '../components/styled-components/Links';
 
 function SettingsLanguageScreen() {
   return (
     <ScrollView>
-      <LanguageLinks>
-        <LanguageLink>
-          <LanguageLabel>Deutsch</LanguageLabel>
-          <Ionicons name="md-checkmark" size={24} />
-        </LanguageLink>
-        <LanguageLink>
-          <LanguageLabel>English</LanguageLabel>
-        </LanguageLink>
-      </LanguageLinks>
+      <Textbox>
+        <RowLink title="Deutsch" name="md-checkmark" />
+        <RowLink title="English" />
+      </Textbox>
     </ScrollView>
   );
 }
@@ -24,20 +19,3 @@ SettingsLanguageScreen.navigationOptions = {
 };
 
 export default SettingsLanguageScreen;
-
-const LanguageLinks = styled.View`
-  padding: 5px 15px;
-  margin: 5px 0;
-  border-bottom-width: 1px;
-  border-bottom-color: #f0f0f0;
-`;
-
-const LanguageLink = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
-`;
-
-const LanguageLabel = styled.Text`
-  padding: 4px 0;
-  font-size: 16px;
-`;
