6457a95477f6d92703bd6715e915e9f08392cae0
AD <atar.dci@gmail.com>
2019-09-01 17:30:05 +0200

    Refactor Amount and Notification Pickers

--- src/screens/ProductFormScreen.js
+++ src/screens/ProductFormScreen.js
@@ -97,6 +97,14 @@ function AddLabels() {
   );
 }
 
+function CreateNumberPicker(max) {
+  const numberPicker = [];
+  for (let i = 1; i <= max; i++) {
+    numberPicker.push(<Picker.Item label={`${i}`} value={i} key={i} />);
+  }
+  return numberPicker;
+}
+
 function ProductFormScreen() {
   return (
     <ScrollView>
@@ -120,11 +128,7 @@ function ProductFormScreen() {
 
       <Text>Menge</Text>
       <Picker prompt="Menge" selectedValue={1}>
-        <Picker.Item label="1" value={1} />
-        <Picker.Item label="2" value={2} />
-        <Picker.Item label="3" value={3} />
-        <Picker.Item label="4" value={4} />
-        <Picker.Item label="5" value={5} />
+        {CreateNumberPicker(5)}
       </Picker>
 
       <Text>Mindesthaltbarkeitsdatum</Text>
@@ -132,20 +136,7 @@ function ProductFormScreen() {
 
       <Text>Benachrichtigung</Text>
       <Picker prompt="Benachrichtigung" selectedValue={1}>
-        <Picker.Item label="1" value={1} />
-        <Picker.Item label="2" value={2} />
-        <Picker.Item label="3" value={3} />
-        <Picker.Item label="4" value={4} />
-        <Picker.Item label="5" value={5} />
-        <Picker.Item label="6" value={6} />
-        <Picker.Item label="7" value={7} />
-        <Picker.Item label="8" value={8} />
-        <Picker.Item label="9" value={9} />
-        <Picker.Item label="10" value={10} />
-        <Picker.Item label="11" value={11} />
-        <Picker.Item label="12" value={12} />
-        <Picker.Item label="13" value={13} />
-        <Picker.Item label="14" value={14} />
+        {CreateNumberPicker(14)}
       </Picker>
 
       <AddLabels />
