b56afca5a75378bdf97c0368a5ed562a1bbd9dad
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-09-01 15:59:29 +0200

    Generalize the button components

--- src/components/styled-components/Buttons.js
+++ src/components/styled-components/Buttons.js
@@ -1,42 +1,21 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled from 'styled-components/native';
+import { Colors } from './Variables';
 
-// Default Button
-const StyledDefaultButton = styled.TouchableOpacity`
-  background-color: #dbd962;
+// Primary Button
+const StyledPrimaryButton = styled.TouchableOpacity`
+  background-color: ${props => props.color || Colors.SecondaryColor};
   padding: 3px 5px;
   margin: 3px 0;
   width: 40%;
   border-radius: 5px;
 `;
-const StyledDefaultButtonText = styled.Text`
+const StyledPrimaryButtonText = styled.Text`
   text-align: center;
   font-size: 18px;
 `;
-export const DefaultButton = props => (
-  <StyledDefaultButton {...props}>
-    <StyledDefaultButtonText>{props.title}</StyledDefaultButtonText>
-  </StyledDefaultButton>
-);
-
-// Save Button
-const StyledSaveButton = styled(DefaultButton)`
-  background-color: #3cb371;
-`;
-const StyledSaveButtonText = styled(DefaultButton)``;
-export const SaveButton = props => (
-  <StyledSaveButton {...props}>
-    <StyledSaveButtonText>{props.title}</StyledSaveButtonText>
-  </StyledSaveButton>
-);
-
-// Cancel Button
-const StyledCancelButton = styled(DefaultButton)`
-  background-color: #ff0000;
-`;
-const StyledCancelButtonText = styled(DefaultButton)``;
-export const CancelButton = props => (
-  <StyledCancelButton {...props}>
-    <StyledCancelButtonText>{props.title}</StyledCancelButtonText>
-  </StyledCancelButton>
+export const PrimaryButton = props => (
+  <StyledPrimaryButton {...props}>
+    <StyledPrimaryButtonText>{props.title}</StyledPrimaryButtonText>
+  </StyledPrimaryButton>
 );
