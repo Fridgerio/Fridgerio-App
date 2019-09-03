5e6ea84227bc70fac2c7772f36612d7fc8fa667b
AD <atar.dci@gmail.com>
2019-09-03 11:46:19 +0200

    Copy styles from ListScreen

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,41 @@
 import React from 'react';
-import { View, TextInput } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 function SearchBar() {
   return (
-    <View>
+    <View style={styles.searchBarContainer}>
       <TextInput
+        style={styles.textInput}
         placeholder={'Suche nach Produkt...'}
         /* callback fn is executed both when user hits return on device keyboard and when keyboard is dismissed - triggered by the TouchableWithoutFeedback wrapper - by clicking anywhere else on the screen */
         onEndEditing={() => console.warn('submitted/dismissed')}
       />
-      <Ionicons name="ios-search" />
+      <Ionicons name="ios-search" style={styles.searchIcon} />
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  searchBarContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    height: 35,
+    backgroundColor: 'whitesmoke',
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  textInput: {
+    flex: 1,
+    paddingHorizontal: 10,
+    fontSize: 16,
+  },
+  searchIcon: {
+    fontSize: 28,
+    color: '#1b4e55',
+    alignSelf: 'center',
+    marginHorizontal: 15,
+    marginTop: 3,
+  },
+});
 export default SearchBar;
