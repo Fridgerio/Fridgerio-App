a2d5fc5dccf9d157a5a8cbb54d85140e86fabec5
kgrhartlage <webdevkonto@gmail.com>
2019-09-03 12:17:40 +0200

    Create dummy data

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { data } from './data';
 
 export let Context;
 // eslint-disable-next-line no-multi-assign
