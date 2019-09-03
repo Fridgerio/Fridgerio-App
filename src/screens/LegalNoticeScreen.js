4dd91bf96c438651ce05489f8bc68419742da41b
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-09-03 16:11:04 +0200

    comments and styling

--- src/screens/LegalNoticeScreen.js
+++ src/screens/LegalNoticeScreen.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { ScrollView, Text, View, StyleSheet } from 'react-native';
 import { Textbox } from '../components/styled-components/Boxes';
 
 function LegalNoticeScreen() {
-  const [legal, setLegal] = useState(null);
-  const [error, setError] = useState(null);
+  /* a few states */
+  const [legal, setLegal] = useState(null); // fetched data
+  const [error, setError] = useState(null); // error
+  /* lifecycle method, such as componentDidMount */
   useEffect(() => {
     fetchLegal();
   }, []);
+  /* method to fetch the legal notice information */
   const fetchLegal = async () => {
     try {
       const url = `https://impressum-api.sklinkusch.now.sh/impressum`;
@@ -18,24 +21,33 @@ function LegalNoticeScreen() {
       setError(err);
     }
   };
+  /* render the component */
   return (
     <ScrollView>
       <Textbox>
+        {/* render the error message if an error occurs*/}
         {error && <Text>{error.message}</Text>}
+        {/* map over the titles */}
         {legal &&
           legal.title.map((subtitle, index) => (
             <View key={`title-${index}`}>
-              <View>
-                <Text>{subtitle}</Text>
+              {/* overall wrapper with a key */}
+              <View style={styles.titleWrapper}>
+                {/* only to make the text a block element  */}
+                <Text style={styles.title}>{subtitle}</Text>
               </View>
               {typeof legal.content[index] === 'string' ? (
                 <View>
+                  {/* only to make the text a block element */}
                   <Text>{legal.content[index]}</Text>
                 </View>
               ) : (
                 legal.content[index].map((paragraph, subindex) => (
-                  <View>
-                    <Text key={`para-${index}-${subindex}`}>{paragraph}</Text>
+                  <View
+                    style={styles.paraWrapper}
+                    key={`para-${index}-${subindex}`}
+                  >
+                    <Text>{paragraph}</Text>
                   </View>
                 ))
               )}
@@ -45,6 +57,19 @@ function LegalNoticeScreen() {
     </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  titleWrapper: {
+    marginTop: 15,
+  },
+  paraWrapper: {
+    marginVertical: 5,
+  },
+});
 
 LegalNoticeScreen.navigationOptions = {
   title: 'Impressum',
