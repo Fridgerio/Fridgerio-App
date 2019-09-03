9935c882d016ec4ae6ccff86db1b9ae80e45fa54
kgrhartlage <webdevkonto@gmail.com>
2019-09-03 12:57:14 +0200

    Add placeholder method to fetch data

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,11 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import { data } from './data';
 
-export let Context;
-// eslint-disable-next-line no-multi-assign
-const { Provider } = (Context = React.createContext(null));
+export const Context = React.createContext(null);
 
-export default function ContextProvider() {
-  return <Provider value={{}}>{props.children}</Provider>;
+export default function ContextProvider({ children }) {
+  const [products, setProducts] = useState(data);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    // getData();
+  }, []);
+
+  const getData = async () => {
+    setError(false);
+    setIsLoading(true);
+
+    const url = '';
+
+    try {
+      const response = await axios.get(url);
+      setProducts(response.data);
+      setIsLoading(false);
+    } catch (err) {
+      console.error(err);
+      setIsLoading(false);
+      setError(true);
+    }
+  };
+  return <Context.Provider value={{ products }}>{children}</Context.Provider>;
 }
