f52d9eb7a93992b21427a1bb41f603e5cda01996
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-09-04 11:09:13 +0200

    render privacy policy declaration

--- src/screens/PrivacyPolicyScreen.js
+++ src/screens/PrivacyPolicyScreen.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { ScrollView, Text } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 import { Textbox } from '../components/styled-components/Boxes';
-import axios from 'axios';
+import { H2, BlockText } from '../components/styled-components/Text';
 
 function PrivacyPolicyScreen() {
   /* a few states */
@@ -15,7 +15,8 @@ function PrivacyPolicyScreen() {
   const fetchPrivacy = async () => {
     try {
       const url = `https://impressum-api.sklinkusch.now.sh/datenschutz`;
-      const data = await axios(url);
+      const response = await fetch(url);
+      const data = await response.json();
       setPrivacy(data);
     } catch (err) {
       setError(err);
@@ -25,17 +26,29 @@ function PrivacyPolicyScreen() {
   return (
     <ScrollView>
       <Textbox>
-        <Text>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-          takimata sanctus est Lorem ipsum dolor sit amet.
-        </Text>
+        {/* render the error message if an error occurs*/}
+        {error && <Text>{error.message}</Text>}
+        {/* map over the titles */}
+        {privacy &&
+          privacy.title.map((subtitle, index) => (
+            <View key={`title-${index}`}>
+              {/* overall wrapper with a key */}
+              {/* only to make the text a block element  */}
+              <H2>{subtitle}</H2>
+              {typeof privacy.content[index] === 'string' ? (
+                <View>
+                  {/* only to make the text a block element */}
+                  <BlockText>{privacy.content[index]}</BlockText>
+                </View>
+              ) : (
+                privacy.content[index].map((paragraph, subindex) => (
+                  <View key={`para-${index}-${subindex}`}>
+                    <BlockText>{paragraph}</BlockText>
+                  </View>
+                ))
+              )}
+            </View>
+          ))}
       </Textbox>
     </ScrollView>
   );
