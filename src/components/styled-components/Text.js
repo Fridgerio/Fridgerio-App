b06a2b77fc65ed254f2ab3a7b7be433c5017b701
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-09-04 19:57:34 +0200

    Update Heading syntax
    Remove props h1-h6

--- src/components/styled-components/Text.js
+++ src/components/styled-components/Text.js
@@ -1,35 +1,30 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import { Colors } from './Variables';
+import { Colors, FontSize } from './Variables';
 
 // Headings
-const h1 = 'font-size: 22px; margin: 25px 0 10px 0;';
-const h2 = 'font-size: 20px; margin: 25px 0 10px 0;';
-const h3 = 'font-size: 19px; margin: 25px 0 10px 0;';
-const h4 = 'font-size: 18px; margin: 25px 0 10px 0;';
-const h5 = 'font-size: 17px; margin: 25px 0 10px 0;';
-const h6 = 'font-size: 16px; margin: 25px 0 10px 0;';
+const h1 = 'font-size: 22px;';
+const h2 = 'font-size: 20px;';
+const h3 = 'font-size: 19px;';
+const h4 = 'font-size: 18px;';
+const h5 = 'font-size: 17px;';
+const h6 = 'font-size: 16px;';
 
 const StyledHeading = styled.Text`
   color: ${props => props.color || Colors.TertiaryColor};
-  ${props => (props.h1 ? h1 : null)}
-  ${props => (props.h2 ? h2 : null)}
-  ${props => (props.h3 ? h3 : null)}
-  ${props => (props.h4 ? h4 : null)}
-  ${props => (props.h5 ? h5 : null)}
-  ${props => (props.h6 ? h6 : null)}
+  font-size: ${props => props.size || FontSize.large};
+  margin: ${props => props.margin || '25px 0 10px 0'};
+  flex: ${props => props.flex || 'none'};
 `;
 
 export const Heading = props => (
   <StyledHeading
-    h1={props.h1}
-    h2={props.h2}
-    h3={props.h3}
-    h4={props.h4}
-    h5={props.h5}
-    h6={props.h6}
+    color={props.color}
+    size={props.size}
+    margin={props.margin}
+    flex={props.flex}
   >
-    {props.children}
+    {props.title}
   </StyledHeading>
 );
 
@@ -38,6 +33,7 @@ export const Heading = props => (
 const StyledBlockText = styled.Text`
   text-align: justify;
   margin: ${props => props.margin || '0'};
+  font-size: ${props => props.size || FontSize.normal};
 `;
 export const BlockText = props => (
   <StyledBlockText margin={props.margin}>{props.children}</StyledBlockText>
