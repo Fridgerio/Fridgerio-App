3a890c56fbf386e2b0c185f10f1007b922b086bc
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-09-05 09:04:46 +0200

    refactor render method

--- src/screens/PrivacyPolicyScreen.js
+++ src/screens/PrivacyPolicyScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { ScrollView, Text } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 import { Textbox } from '../components/styled-components/Boxes';
+import { H2, BlockText } from '../components/styled-components/Text';
 
 function PrivacyPolicyScreen() {
   /* a few states */
@@ -24,20 +25,35 @@ function PrivacyPolicyScreen() {
     }
     setLoading(false);
   };
+  const loadMessage = 'Lade Daten...';
   return (
     <ScrollView>
       <Textbox>
-        <Text>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-          takimata sanctus est Lorem ipsum dolor sit amet.
-        </Text>
+        {/* render the loading message if the data is loading */}
+        {loading && <Text>{loadMessage}</Text>}
+        {/* render the error message if an error occurs*/}
+        {error && <Text>{error.message}</Text>}
+        {/* map over the titles */}
+        {privacy &&
+          privacy.title.map((subtitle, index) => (
+            <View key={`title-${index}`}>
+              {/* overall wrapper with a key */}
+              {/* only to make the text a block element  */}
+              <H2>{subtitle}</H2>
+              {typeof privacy.content[index] === 'string' ? (
+                <View>
+                  {/* only to make the text a block element */}
+                  <BlockText>{privacy.content[index]}</BlockText>
+                </View>
+              ) : (
+                privacy.content[index].map((paragraph, subindex) => (
+                  <View key={`para-${index}-${subindex}`}>
+                    <BlockText>{paragraph}</BlockText>
+                  </View>
+                ))
+              )}
+            </View>
+          ))}
       </Textbox>
     </ScrollView>
   );
