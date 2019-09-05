b961eeef9fcb14e8e17bbb293528b604d0134f83
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-09-05 08:45:04 +0200

    a few methods

--- src/screens/PrivacyPolicyScreen.js
+++ src/screens/PrivacyPolicyScreen.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ScrollView, Text } from 'react-native';
 import { Textbox } from '../components/styled-components/Boxes';
 
 function PrivacyPolicyScreen() {
   /* a few states */
-  const [legal, setLegal] = useState(null); // fetched data
+  const [privacy, setPrivacy] = useState(null); // fetched data
   const [error, setError] = useState(null); // error
   const [loading, setLoading] = useState(false); // loading status
+  /* lifecycle method, such as componentDidMount */
+  useEffect(() => {
+    fetchPrivacy();
+  }, []);
+  /* method to fetch the privacy policy information */
+  const fetchPrivacy = async () => {
+    setLoading(true);
+    try {
+      const url = `https://impressum-api.sklinkusch.now.sh/datenschutz`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setPrivacy(data);
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  };
   return (
     <ScrollView>
       <Textbox>
