842bc1232dd642be22ea0f029eb3257ec35b0991
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-09-05 10:18:46 +0200

    implement fetch

--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -17,11 +17,25 @@ function CameraScreen({ navigation }) {
     };
     askPermission();
   }, []);
+  const fetchProduct = async code => {
+    const url = `https://products.sklinkusch.now.sh/?${code}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    const {
+      status,
+      product: { product_name, quantity },
+    } = await data;
+    if (status === 1) {
+      alert(`Bar code ${code} recognized, corresponding to ${product_name} ${quantity}`);
+    } else {
+      alert(`Bar code ${code} is not in the database`);
+    }
+  };
   /* Alert with barcode type and number */
   const handleBarCodeScanned = ({ type, data }) => {
     if (type === 32 || type === 64) {
       toggleScanned(true);
-      alert(`Bar code with type ${type} and data ${data} has been scanned.`);
+      fetchProduct(data);
     } else {
       toggleScanned(false);
     }
