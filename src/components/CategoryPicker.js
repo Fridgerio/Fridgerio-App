73b0a1984428ea0b2a1a4aeb0a08fad1772b46e2
AD <atar.dci@gmail.com>
2019-09-09 15:37:47 +0200

    Refactor Category Picker using external library

--- src/components/CategoryPicker.js
+++ src/components/CategoryPicker.js
@@ -1,77 +1,28 @@
 import React from 'react';
-import { Picker, Modal, View } from 'react-native';
-
-import { Input } from '../components/styled-components/Inputs';
-import { TouchableHighlight } from 'react-native-gesture-handler';
+import { Text } from 'react-native';
+import { Textbox } from './styled-components/Boxes';
+import RNPickerSelect from 'react-native-picker-select';
 
 const categories = [
-  { id: '1', name: 'Alle', icon: 'food' },
-  { id: '2', name: 'Obst&Gemüse', icon: 'food-apple' },
-  { id: '3', name: 'Milchprodukte', icon: 'food-croissant' },
-  { id: '4', name: 'Nudeln, Reis usw.', icon: 'food-fork-drink' },
-  { id: '5', name: 'Getränke', icon: 'food-variant' },
+  { value: '1', label: 'Alle', icon: 'food' },
+  { value: '2', label: 'Obst&Gemüse', icon: 'food-apple' },
+  { value: '3', label: 'Milchprodukte', icon: 'food-croissant' },
+  { value: '4', label: 'Nudeln, Reis usw.', icon: 'food-fork-drink' },
+  { value: '5', label: 'Getränke', icon: 'food-variant' },
 ];
 
 // Returns picker with category names
-export default class CategoryPicker extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visible: false,
-    };
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <TouchableHighlight
-          onPress={() => {
-            console.log(this.state.visible);
-            return this.setState(prev => ({ visible: !prev.visible }));
-          }}
-        >
-          <Input
-            inputLabel="Kategorie"
-            placeholder="Bitte wähle eine Kategorie"
-            editable={false}
-            defaultValue={categories[1].name}
-          />
-        </TouchableHighlight>
-        <Modal
-          visible={this.state.visible}
-          transparent
-          onRequestClose={() => this.setState({ visible: false })}
-        >
-          <View
-            style={{
-              position: 'absolute',
-              bottom: 0,
-              right: 0,
-              left: 0,
-              height: 200,
-              backgroundColor: 'whitesmoke',
-            }}
-          >
-            <Picker
-              prompt="Kategorie"
-              selectedValue={categories[1].id}
-              onValueChange={itemValue => {
-                console.warn(`Category ${itemValue} selected`);
-                return this.setState({ visible: false });
-              }}
-            >
-              <Picker.Item label="Bitte wähle eine Kategorie" value="0" />
-              {categories.map(category => (
-                <Picker.Item
-                  label={category.name}
-                  value={category.id}
-                  key={category.id}
-                />
-              ))}
-            </Picker>
-          </View>
-        </Modal>
-      </React.Fragment>
-    );
-  }
+export default function CategoryPicker() {
+  return (
+    <React.Fragment>
+      <Textbox>
+        <Text>Kategorie</Text>
+      </Textbox>
+      <RNPickerSelect
+        onValueChange={value => console.warn(value)}
+        items={categories}
+        placeholder={{ label: 'Bitte wähle eine Kategorie', value: null }}
+      />
+    </React.Fragment>
+  );
 }
