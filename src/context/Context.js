4dacb2f956f98e84a0aee4fab183d6562d426935
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-09-10 11:34:00 +0200

    get data from db on mount

--- src/context/Context.js
+++ src/context/Context.js
@@ -5,31 +5,25 @@ import { SQLite } from 'expo-sqlite';
 import { data } from './data';
 
 export const Context = React.createContext(null);
+const db = SQLite.openDatabase('products.db');
 
 export default function ContextProvider({ children }) {
-  const [products, setProducts] = useState(data);
+  const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    // getData();
+    db.transaction(tr =>
+      tr.executeSql('CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY NOT NULL, name TEXT(40), amount TINYINT, category TEXT(100), label TEXT(75), bbdate DATE, notification TINYINT, note TEXT)'));
+    getData();
   }, []);
 
-  const getData = async () => {
+  const getData = () => {
     setError(false);
     setIsLoading(true);
-
-    const url = '';
-
-    try {
-      const response = await axios.get(url);
-      setProducts(response.data);
-      setIsLoading(false);
-    } catch (err) {
-      console.error(err);
-      setIsLoading(false);
-      setError(true);
-    }
+    db.transaction(tr =>
+      tr.executeSql('SELECT * FROM QUOTES', [], (tx, res) =>
+        setProducts(res.rows._array)));
   };
   return <Context.Provider value={{ products }}>{children}</Context.Provider>;
 }
