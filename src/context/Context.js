931070c5d542c2bf8d646be404bb5f67118fefa9
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-09-10 11:23:40 +0200

    import SQLite

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
+import { SQLite } from 'expo-sqlite';
 import { data } from './data';
 
 export const Context = React.createContext(null);
