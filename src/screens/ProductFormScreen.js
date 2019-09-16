112c6a2a8390f767a77e656bb845122765525488
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-09-16 10:53:58 +0200

    add function clearForm

--- src/screens/ProductFormScreen.js
+++ src/screens/ProductFormScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useRef } from 'react';
 import { ScrollView, Text, FlatList } from 'react-native';
 
 import CategoryPicker from '../components/CategoryPicker';
@@ -8,11 +8,32 @@ import BestBeforeDatePicker from '../components/BestBeforeDatePicker';
 import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 import { PrimaryButton } from '../components/styled-components/Buttons';
 import { Input } from '../components/styled-components/Inputs';
+import { Context } from '../context/Context';
 
 function ProductFormScreen({ navigation }) {
   const product = navigation.state.params;
   const [name, setName] = useState(product ? product.name : null);
   const [categories, setCategories] = useState(product ? product.categories : []);
+  const { addProduct } = useContext(Context);
+  const inputField = useRef(null);
+  const categorySelector = useRef(null);
+  const amountField = useRef(null);
+  const dateSelector = useRef(null);
+  const notificationSelector = useRef(null);
+  const labelSelector = useRef(null);
+  const notesField = useRef(null);
+  const clearForm = () => {
+    const fields = [
+      inputField,
+      categorySelector,
+      amountField,
+      dateSelector,
+      notificationSelector,
+      labelSelector,
+      notesField,
+    ];
+    fields.forEach(field => (field.current.value = ''));
+  };
   console.log(product);
   return (
     <ScrollView>
@@ -36,17 +57,22 @@ function ProductFormScreen({ navigation }) {
         placeholder="z.B. Apfel"
         defaultValue={name}
         editable
+        ref={inputField}
       />
 
-      <CategoryPicker category={categories[0]} />
+      <CategoryPicker category={categories[0]} ref={categorySelector} />
 
-      <NumberPicker title="Menge" maxNum={10} />
+      <NumberPicker title="Menge" maxNum={10} ref={amountField} />
 
-      <BestBeforeDatePicker />
+      <BestBeforeDatePicker ref={dateSelector} />
 
-      <NumberPicker title="Benachrichtigung" maxNum={14} />
+      <NumberPicker
+        title="Benachrichtigung"
+        maxNum={14}
+        ref={notificationSelector}
+      />
 
-      <AddLabels />
+      <AddLabels ref={labelSelector} />
 
       <Input
         inputLabel="Notiz"
@@ -54,12 +80,13 @@ function ProductFormScreen({ navigation }) {
         multiline
         editable
         textAlignVertical="top"
+        ref={notesField}
       />
 
       <FlatList
         data={[
-          { key: 'x', name: 'Abbrechen' },
-          { key: 'v', name: 'Speichern' },
+          { key: 'x', name: 'Abbrechen', function: { clearForm } },
+          { key: 'v', name: 'Speichern', function: { addEditProduct } },
         ]}
         keyExtractor={item => item.key}
         renderItem={({ item }) => <PrimaryButton title={item.name} />}
