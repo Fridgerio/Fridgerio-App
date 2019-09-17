3c48f3f02f51da26e589732b4ed402124a9b4a5c
AD <atar.dci@gmail.com>
2019-09-17 17:41:29 +0200

    Style Text

--- src/components/CameraHelpText.js
+++ src/components/CameraHelpText.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
-import { Text } from 'react-native';
+import { StyledText } from './styled-components/Text.js';
 
 export default function HelpText() {
   const [help, setHelp] = useState(false);
   setTimeout(() => {
     setHelp(true);
   }, 6000);
-  return help ? <Text>Help</Text> : <Text />;
+  return help ? (
+    <StyledText color="white" margin="0 auto">
+      Position a product barcode in the frame
+    </StyledText>
+  ) : (
+    <StyledText />
+  );
 }
