b46e3c63064a2161f9f41144a42707726429473c
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-09-19 11:21:13 +0200

    fix statistics function

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -17,14 +17,16 @@ function Expire() {
 /* Statistics */
 function Statistics({ products }) {
   const getExpired = id => {
-    const normDate = new Date(Date.now() + id * 24 * 60 * 60 * 1000);
-    const today = new Date(Date.now());
+    const normDate = Number((
+        new Date(Date.now() + id * 24 * 60 * 60 * 1000).getTime() / 1000
+      ).toFixed(0));
+    const today = Number((new Date(Date.now()).getTime() / 1000).toFixed(0));
     let number;
     if (products && products.length > 0) {
       number = products.reduce((num, current) => {
-        const expDate = Number((new Date(current.date).getTime() / 1000).toFixed(0));
+        const expDate = Number((new Date(current.bestBeforeDate).getTime() / 1000).toFixed(0));
         if (id === 0) {
-          return expDate < normDate ? num + 1 : num;
+          return expDate < today ? num + 1 : num;
         }
         return expDate < normDate && expDate > today ? num + 1 : num;
       }, 0);
