b1f053200b90d42db7591423e5e1a3e32533361c
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-09-23 10:44:14 +0200

    make error messages, fix #77

--- src/screens/ProductFormScreen.js
+++ src/screens/ProductFormScreen.js
@@ -9,6 +9,7 @@ import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 import { PrimaryButton } from '../components/styled-components/Buttons';
 import { Input } from '../components/styled-components/Inputs';
 import { Context } from '../context/Context';
+import { BlockText } from '../components/styled-components/Text';
 
 function ProductFormScreen({ navigation }) {
   const product = navigation.state.params;
@@ -20,6 +21,7 @@ function ProductFormScreen({ navigation }) {
   const [bestBeforeDate, setBestBeforeDate] = useState(null);
   const [pushNotificationDate, setPushNotificationDate] = useState(null);
   const [customNote, setCustomNote] = useState(null);
+  const [error, setError] = useState(null);
   const { addProduct } = useContext(Context);
   const getNotificationDate = days => {
     const notificationDate = new Date(bestBeforeDate - days * 24 * 60 * 60 * 1000).toLocaleDateString('de-DE');
@@ -38,15 +40,28 @@ function ProductFormScreen({ navigation }) {
     setCustomNote(null);
   };
   const addEditProduct = () => {
-    addProduct(
-      productName,
-      amount,
-      productCategory,
-      bestBeforeDate,
-      pushNotificationDate,
-      customNote,
-      null
-    );
+    if (
+      productName === null ||
+      productName === undefined ||
+      productName === ''
+    ) {
+      setError('Es wurde kein Name angegeben.');
+    } else if (amount === null || amount === undefined || amount === '') {
+      setError('Es wurde keine Menge angegeben.');
+    } else {
+      setError(null);
+    }
+    if (error !== null) {
+      addProduct(
+        productName,
+        amount,
+        productCategory,
+        bestBeforeDate,
+        pushNotificationDate,
+        customNote,
+        null
+      );
+    }
   };
   // console.log(product);
   return (
@@ -99,6 +114,12 @@ function ProductFormScreen({ navigation }) {
         textAlignVertical="top"
         onChangeText={text => setCustomNote(text)}
       />
+
+      {error && (
+        <BlockText color={'red'} weight={'bold'}>
+          {error}
+        </BlockText>
+      )}
 
       <FlatList
         data={[
