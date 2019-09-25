7e2331230f30164f99974149c2cae4bc2ac6efdd
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-09-25 09:48:41 +0200

    fix error if date is null or undefined

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -1,7 +1,10 @@
 /*  transform date format */
 export function dateFormat(date) {
-  const dateOriginalArray = date.split('-');
-  const dateReverseArray = dateOriginalArray.reverse();
-  const newDateString = dateReverseArray.join('.');
-  return newDateString;
+  if (date) {
+    const dateOriginalArray = date.split('-');
+    const dateReverseArray = dateOriginalArray.reverse();
+    const newDateString = dateReverseArray.join('.');
+    return newDateString;
+  }
+  return null;
 }
